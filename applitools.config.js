c70262266dff233a60b8aae8af5bcc15f3d48f71
Abdur Rehman <rehmanuet@yahoo.com>
2020-12-11 15:00:58 +0500

    Hackathon commit

---
+++ applitools.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testConcurrency: 1,
+    apiKey: 'c3oeHoStwy88ju4ebgfvndGD36lOhFURPos1JxlRkFU110',
+    browser: [
+        {width: 1200, height: 800, name: 'chrome'}
+    ],
+    batchName: 'Holiday Shopping'
+}
---
+++ cypress/integration/tcv.js
@@ -0,0 +1,61 @@
+/// <reference types="cypress" />
+
+
+describe("Holiday Shopping Hackathon", () => {
+    beforeEach(() => {
+        cy.visit('https://demo.applitools.com/tlcHackathonMasterV1.html')
+    })
+    afterEach(() => {
+        cy.eyesClose()
+    })
+
+    it(`Task 1 - Main Page `, function() {
+
+
+        cy.eyesOpen({
+            appName: 'AppliFashion',
+            testName: 'Test 1'
+        })
+
+        cy.eyesCheckWindow({
+            tag: "main page",
+            target: 'window',
+            fully: true
+        });
+
+    });
+
+    it(`Task 2 - Filtered Product Grid `, function() {
+
+        cy.eyesOpen({
+            appName: 'AppliFashion',
+            testName: 'Test 2'
+        })
+        cy.get('#colors__Black').click()
+        cy.get('#filterBtn').click()
+
+
+        cy.eyesCheckWindow({
+            tag: "filter by color",
+            target: 'region',
+            selector: '#product_grid'
+        });
+
+    });
+
+    it(`Task 3 - Product Details`, function() {
+
+
+        cy.eyesOpen({
+            appName: 'AppliFashion',
+            testName: 'Task 3'
+        })
+
+        cy.get('#IMG__imgfluid__215').click()
+        cy.eyesCheckWindow({
+            tag: "product details",
+            target: 'window',
+            fully: true
+        });
+    });
+});
