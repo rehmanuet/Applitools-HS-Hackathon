752366bb9cd7a321791ea4ca8f39afc3e2afba77
Abdur Rehman <rehmanuet@yahoo.com>
2020-12-12 23:05:53 +0500

    Added Docs

--- cypress/integration/shopping.e2e.test.js
+++ cypress/integration/shopping.e2e.test.js
@@ -1,16 +1,18 @@
-/// <reference types="cypress" />
-
+/// <reference types="@applitools/eyes-cypress" />
 
 describe("Holiday Shopping Hackathon", () => {
+
+    // Opens different Applifashion App Versions via command-line
     beforeEach(() => {
         cy.visit(Cypress.env('app_version'))
     })
+
+    // Closes the eyes instance after each test
     afterEach(() => {
         cy.eyesClose()
     })
 
     it(`Task 1 - Main Page `, function() {
-
 
         cy.eyesOpen({
             appName: 'AppliFashion',
@@ -31,9 +33,12 @@ describe("Holiday Shopping Hackathon", () => {
             appName: 'AppliFashion',
             testName: 'Test 2'
         })
-        cy.get('#colors__Black').click()
-        cy.get('#filterBtn').click()
 
+        // Checks nable to black color filter
+        cy.get('#colors__Black').click()
+
+        // Clicks on filter button
+        cy.get('#filterBtn').click()
 
         cy.eyesCheckWindow({
             tag: "filter by color",
@@ -45,13 +50,14 @@ describe("Holiday Shopping Hackathon", () => {
 
     it(`Task 3 - Product Details`, function() {
 
-
         cy.eyesOpen({
             appName: 'AppliFashion',
             testName: 'Task 3'
         })
 
+        //Opens Appli Air x Night shoe without filtering
         cy.get('#IMG__imgfluid__215').click()
+
         cy.eyesCheckWindow({
             tag: "product details",
             target: 'window',
