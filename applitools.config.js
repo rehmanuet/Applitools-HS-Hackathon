1126ca12152f74649386dc9514718f4946378082
Abdur Rehman <rehmanuet@yahoo.com>
2020-12-12 22:46:03 +0500

    UFG:Task

--- applitools.config.js
+++ applitools.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    testConcurrency: 1,
-    apiKey: 'c3oeHoStwy88ju4ebgfvndGD36lOhFURPos1JxlRkFU110',
+    testConcurrency: 4,
     browser: [
-        {width: 1200, height: 800, name: 'chrome'}
+        {width: 1200, height: 800, name: 'chrome'},
+        {width: 1200, height: 800, name: 'firefox'},
+        {width: 1200, height: 800, name: 'edgechromium'},
+        {width: 1200, height: 800, name: 'safari'},
+        {deviceName: 'iPhone X', screenOrientation: 'portrait'}
     ],
     batchName: 'Holiday Shopping'
 }
